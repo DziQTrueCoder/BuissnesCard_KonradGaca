8fb3cefc644bd9a96f986f75c9f34d4ecb620489
DziQ-php <konradgaca04@gmail.com>
2021-08-28 13:26:58 +0200

    Add new files

---
+++ index.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="uft-8">
+        <title>DziQ-Web-Developer</title>
+        <meta name= description content = "psycholog">
+        <meta name="keywords" content= "psycholog,września,opieszyn,pomoc">
+        <meta name ="author" content="Konrad Gaca">
+        <meta http-equiv="X-Ua-Compatible" content="IE=edge,chrome=1">
+        <link rel="stylesheet" href="">
+        <script src=></script>
+        <meta http-equiv="refresh" content="x">
+        <meta http-equiv="Expires" content="0" />
+	<meta http-equiv="Cache-Control" content="no-store, no-cache, must-revalidate" />
+	<meta http-equiv="Cache-Control" content="post-check=0, pre-check=0" />
+	<meta http-equiv="Pragma" content="no-cache" />
+    <link rel="stylesheet" href="class.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css">
+</head>
+<body>
+<header>
+    <div class="Welcome">
+        <p> Witaj</p>
+        <h1>Konrad Gaca</h1>
+        <h2>Web developer - freelancer</h2>
+        <a href="portfolio.php"target="_blank">
+        <button>Zobacz moje portfolio</button>
+        
+        
+</div>
+</header>
+<section class="features clearfix">
+    <h1>Tworze ładne strony internetowe</h1>
+    <h2>Szybki czas dostarczenia usługi</h2>
+    <h2>Dobry kontakt z klientem</h2>
+    <h2>Moje strony dobrze wyglądają na urządzeniach mobilnych</h2>
+
+</section>
+<section class="contact">
+        <h1>Skontaktuj się ze mną</h1>
+        <div class="wrap clearfix">
+            <form>
+                <input type="text" placeholder="Twoje imię">
+                <input type="text" placeholder="Email">
+                <textarea placeholder="Twoja wiadomość"></textarea>
+                <button>Wyślij wiadomość</button>
+            </form>
+
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
